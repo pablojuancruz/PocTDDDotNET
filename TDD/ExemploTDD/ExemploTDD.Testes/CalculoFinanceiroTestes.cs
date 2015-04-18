14dd0ad561bb52b7d5dc57dcb50c52b4019188b7
pablojuancruz <pablojuan@foryoursystem.com>
2015-04-18 16:23:47 -0300

    Commit da Aplicação

---
+++ TDD/ExemploTDD/ExemploTDD.Testes/CalculoFinanceiroTestes.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ExemploTDD;
+
+namespace ExemploTDD.Testes
+{
+    [TestClass]
+    public class CalculoFinanceiroTestes
+    {
+        [TestMethod]
+        public void Teste01()
+        {
+            double valorAPagar = CalculoFinanceiro.CalcularValorComJurosCompostos(10000, 12, 2);
+            Assert.AreEqual(valorAPagar,12682.42);
+        }
+
+        [TestMethod]
+        public void Teste02()
+        {
+            double valorAPagar = CalculoFinanceiro.CalcularValorComJurosCompostos(11937.28, 24, 4);
+            Assert.AreEqual(valorAPagar, 30598.88);
+        }
+
+        [TestMethod]
+        public void Teste03()
+        {
+            double valorAPagar = CalculoFinanceiro.CalcularValorComJurosCompostos(15000, 36, 6);
+            Assert.AreEqual(valorAPagar, 122208.78);
+        }
+    }
+}
---
+++ TDD/ExemploTDD/ExemploTDD/CalculoFinanceiro.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploTDD
+{
+    public class CalculoFinanceiro
+    {
+
+        public static double CalcularValorComJurosCompostos(double valorEmprestimo, int numMeses, double percTaxa)
+        {
+
+            return  Math.Round(valorEmprestimo * Math.Pow(1 + (percTaxa / 100), numMeses), 2);
+        }
+    }
+}
+
+
+
